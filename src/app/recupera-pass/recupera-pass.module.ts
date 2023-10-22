fb791c474be58eb5151d4445c32a4cf11db21ea6
mbertero <macabertero@gmail.com>
2023-10-21 21:41:08 -0300

    Se agrega recupera-pass

--- src/app/recupera-pass/recupera-pass.module.ts
+++ src/app/recupera-pass/recupera-pass.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { RecuperaPassPageRoutingModule } from './recupera-pass-routing.module';
 
 import { RecuperaPassPage } from './recupera-pass.page';
+import { HeaderModule } from '../header/header.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RecuperaPassPageRoutingModule
+    RecuperaPassPageRoutingModule,
+    ReactiveFormsModule,
+    HeaderModule,
   ],
   declarations: [RecuperaPassPage]
 })
--- src/app/recupera-pass/recupera-pass.page.ts
+++ src/app/recupera-pass/recupera-pass.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StateService } from '../state/state.service';
 
 @Component({
   selector: 'app-recupera-pass',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperaPassPage implements OnInit {
 
-  constructor() { }
+  formularioRecuperacion: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router, private stateService: StateService) { 
+    this.formularioRecuperacion = this.fb.group(
+      {
+        nombre: [""]
+      }
+    )
+  }
 
   ngOnInit() {
   }
+
+  irARecuperar() {
+    const usuario = {
+      nombre: this.formularioRecuperacion.get('nombre')?.value,
+    }
+
+    if(usuario.nombre === ''){
+      alert('Debes ingresar tu nombre de usuario');
+      return;
+    }
+
+    const nombre = usuario.nombre;
+
+    this.stateService.setNombre = 'invitado'
+    this.stateService.setTitulo = 'Homepage'
+    this.router.navigate(['login']);
+  }
+
 
 }
